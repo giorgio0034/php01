2c331f5f5d7169900f3825cd1a4486e90c5ca543
giorgio0034 <152870852+giorgio0034@users.noreply.github.com>
2026-01-28 18:55:26 +0100

    Add files via upload

---
+++ php_01_Giorgio_Gaudio/index.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+$string="io sono Giorgio";
+const STRING= "io sono Giorgio";
+$number=3;
+const NUMBER=3;
+
+$float=23.5;
+const FLOAT=23.5;
+
+$bool=true;
+const BOOL=true;
+
+
+var_dump($string);
+var_dump($number);
+var_dump($float);
+var_dump($bool);
+
+
+
+
+
+?>
