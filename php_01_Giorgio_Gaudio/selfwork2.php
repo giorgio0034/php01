9395f8558adc59551d027fa46ac4a8aa7f6cb413
giorgio0034 <152870852+giorgio0034@users.noreply.github.com>
2026-01-28 19:16:26 +0100

    Add files via upload

---
+++ php_01_Giorgio_Gaudio/selfwork2.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$text = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6= 'hai';
+$text7  = 'bevuto';
+$text8 = "tutto";
+
+echo $text, " ", $text2, " ", $text3, " ", $text4, " ", $text5, " ", $text6, " ", $text7;
+
+
+
+
+
+
+
+
+
+
+?>
