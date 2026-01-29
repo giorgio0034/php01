62e41bcae2d327a99c9bdb3ab3d0ad62d638b1f3
giorgio0034 <152870852+giorgio0034@users.noreply.github.com>
2026-01-29 14:18:13 +0100

    Add files via upload

---
+++ php_01_Giorgio_Gaudio/selfwork3.php
@@ -0,0 +1,48 @@
+<?php
+
+$words1 = [
+  'vostro',
+  'Nel',
+  67,
+  'essere',
+  'colle',
+  'mi',
+  'sempre',
+[
+  'oscura',
+  'era',
+  89,
+  [
+  'mezzo',
+   'E'
+  ],
+'ritrovai',
+'per'
+],
+'diritta'
+];
+
+$words2 = [
+  'elemento1' => 25.89,
+  'elemento2' => 'possa',
+  'elemento3' => [
+    'Virgilio',
+    'Favore',
+    'favore',
+    ['fortuna']
+  ],
+ 'fine' => '!'
+];
+
+print_r($words1);
+echo $words1[1]," ", $words1[7][3][0], " del", " camin di nostra vita ", $words1[5]," " ,$words1[7][4]," per una selva ",$words1[7][0],
+" che la ", $words1[8]," via ",$words1[7][1]," smarrita";
+
+
+
+
+
+
+
+
+?>
